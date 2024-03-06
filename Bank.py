94e60db0712a12589cb3e6b4e391026ff516ac5e
Damien-journel <journeldamien@gmail.com>
2024-03-06 15:14:04 +0100

    Ajout des class Compte et Bank

---
+++ Bank.py
@@ -0,0 +1,9 @@
+class Bank:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def transfer(self):
+        self.balance = self.balance
+        return self.balance
+    def
---
+++ Compte.py
@@ -0,0 +1,5 @@
+class Compte(object):
+    def __init__(self,balance, utilisateur, bank):
+        self.utilisateur =
+        self.bank =
+        self.balance = balance
